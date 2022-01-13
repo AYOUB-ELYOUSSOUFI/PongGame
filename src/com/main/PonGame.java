962c5a3aeb62245e9c3cbe847f1585c1c5c570e2
ayoub elyoussoufi <elyousfiayoub12@gmail.com>
2022-01-13 11:25:09 +0100

    Pong Game

---
+++ src/com/main/PonGame.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax .swing.*;
+
+public class PonGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+
+	}
+
+}
